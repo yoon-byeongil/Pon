b604b324a9f124199ab462de01ff69a7de7ad90f
yoon-byeongil <justbrand101@gmail.com>
2026-06-15 15:41:28 +0900

    Add toggle completion feature

--- Pon/ContentView.swift
+++ Pon/ContentView.swift
@@ -8,7 +8,7 @@ struct Todo: Identifiable {
 
 struct ContentView: View {
     
-    var todos: [Todo] = [
+    @State var todos: [Todo] = [
         Todo(title: "운동하기", isCompleted: false),
         Todo(title: "장보기", isCompleted: true),
         Todo(title: "책 읽기", isCompleted: true)
@@ -18,10 +18,16 @@ struct ContentView: View {
     var body: some View {
         List(todos) { todo in
             HStack {
-                if todo.isCompleted {
-                    Image(systemName: "checkmark.circle.fill")
-                } else {
-                    Image(systemName: "circle")
+                Button(action: {
+                    if let indextodo = todos.firstIndex(where: { $0.id == todo.id }) {
+                        todos[indextodo].isCompleted.toggle()
+                    }
+                }) {
+                    if todo.isCompleted {
+                        Image(systemName: "checkmark.circle.fill")
+                    } else {
+                        Image(systemName: "circle")
+                    }
                 }
                 Text(todo.title)
             }
