5db2921697441f120c252cae1c9f29185f4763e6
yoon-byeongil <justbrand101@gmail.com>
2026-06-23 01:06:41 +0900

    Add AboutView

--- Pon/View/AboutView.swift
+++ Pon/View/AboutView.swift
@@ -1,16 +1,29 @@
-//
-//  AboutView.swift
-//  Pon
-//
-//  Created by 윤병일 on 2026/06/22.
-//
-
 import SwiftUI
 
 struct AboutView: View {
     var body: some View {
-        Text("プログラマー")
-            .font(.title)
+        ZStack {
+            Color(UIColor.systemGroupedBackground)
+                .ignoresSafeArea()
+            
+            VStack {
+                Circle()
+                    .fill(Color.accentColor)
+                    .frame(width: 150, height: 150)
+                    .padding()
+                Text("ポン")
+                    .font(.largeTitle)
+                Text("Ver : 1.0")
+                    .font(.title3)
+                    .padding(.bottom)
+                Text("世界一素早いタスク管理。")
+                    .font(.headline)
+                    .foregroundStyle(.gray)
+                Text("一瞬での追加とチェック。")
+                    .font(.headline)
+                    .foregroundStyle(.gray)
+            }
+        }
     }
 }
 
